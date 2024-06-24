14e3c32c1860ac64f23a58d228d76ccedcd2da97
SaliduMaschine <111045919+DamianEisenring@users.noreply.github.com>
2024-06-24 08:56:15 +0200

    initial commit

---
+++ src/Components/Navbar.js
@@ -0,0 +1,13 @@
+import './Navbar.css';
+
+function App() {
+  return (
+    <div>
+      <Navbar className="nav">
+        <a>Navbur</a>
+      </Navbar>
+    </div>
+  );
+}
+
+export default App;
