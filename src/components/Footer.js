536d923a69b94ac8e4e5f20cc32452f21b03decc
SaliduMaschine <111045919+DamianEisenring@users.noreply.github.com>
2024-08-09 11:11:13 +0200

    home page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,33 +1,31 @@
-import React from "react";
+import React from 'react';
 import {
   FaPhone,
   FaEnvelope,
   FaInstagram,
   FaFacebook,
   FaTwitter,
-} from "react-icons/fa";
-import "./Footer.css"; // Falls du CSS in einer separaten Datei hast
+} from 'react-icons/fa';
+import './Footer.css'; // Falls du CSS in einer separaten Datei hast
 
 const Footer = () => {
   return (
     <footer className="footer">
       <div className="footer-left">
         <h3>Kontakt</h3>
-        <p>Patrick Strehse</p>
-        <p>Geissfluhweg 17</p>
-        <p>5018 Erlinsbach </p>
-        <p>079 963 58 47</p>
-        <p>patrick-j@windowslive.jom</p>
+        <div className="contact-info">
+          <p>Patrick Strehse</p>
+          <p>Geissfluhweg 17</p>
+          <p>5018 Erlinsbach </p>
+          <p>079 963 58 47</p>
+          <p>patrick-j@windowslive.jom</p>
+        </div>
       </div>
       <div className="footer-right">
-        <p>Folge mir</p>
+        <h3>Folge mir</h3>
         <div className="social-icons">
-          <a href="https://www.instagram.com/vieiratiago91/">
-            <FaInstagram />
-          </a>
-          <a href="https://www.facebook.com/people/Tiago-Vieira/100063557804042/">
-            <FaFacebook />
-          </a>
+          <a href="https://www.instagram.com/vieiratiago91/"><FaInstagram /></a>
+          <a href="https://www.facebook.com/people/Tiago-Vieira/100063557804042/"><FaFacebook /></a>
         </div>
       </div>
       <div className="copyright">
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,57 @@
-import React from 'react';
-import './Home.css'
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Home.css"; // Stil für die Home-Seite
 
 const Home = () => {
   return (
     <div>
-      <h1>Willkommen auf der Homepage</h1>
-      <p>Hier könnte der Inhalt deiner Homepage stehen.</p>
+      <div className="home-container">
+        <div className="hero-section">
+          <div className="hero-text">
+            <h1>Viera Tiago</h1>
+            <p>Schwinger</p>
+          </div>
+        </div>
+      </div>
+      <br />
+      <br />
+      <br />
+      <br />
+      <div className="second-home-container">
+        <div className="second-section">
+          <div className="second-text">
+            <h2>Portrait</h2>
+            <h3>Über mich und meine Schwingkarriere</h3>
+            <Link to="/portrait" className="btn">
+              Zum Portrait
+            </Link>
+          </div>
+        </div>
+      </div>
+
+      <div className="third-home-container">
+        <img src="/image/home_to_sponsoren.png" alt="Logo" />
+        <div>
+          <h1>Unterstütze mich auf meinem Weg</h1>
+          <Link to="/partner" className="btn">
+            Mehr Erfahren
+          </Link>
+        </div>
+      </div>
+      <div className="fourth-home-container">
+        <a href="https://www.jaisli-beck.ch/" target="_blank" rel="noopener noreferrer">
+          <img src="/image/jaisli-beck_logo.png" alt="Jaisli-Beck" />
+        </a>
+        <a href="https://www.jaisli-beck.ch/" target="_blank" rel="noopener noreferrer">
+          <img src="/image/jaisli-beck_logo.png" alt="Jaisli-Beck" />
+        </a>
+        <a href="https://www.jaisli-beck.ch/" target="_blank" rel="noopener noreferrer">
+          <img src="/image/jaisli-beck_logo.png" alt="Jaisli-Beck" />
+        </a>
+        <a href="https://www.jaisli-beck.ch/" target="_blank" rel="noopener noreferrer">
+          <img src="/image/jaisli-beck_logo.png" alt="Jaisli-Beck" />
+        </a>
+      </div>
     </div>
   );
 };
